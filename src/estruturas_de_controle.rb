c110d539c02a818165010624fab43054f63a5668
danielpump <new_daniel2010@hotmail.com>
2014-12-08 17:52:14 -0200

    Utilizando o IF

--- src/estruturas_de_controle.rb
+++ src/estruturas_de_controle.rb
@@ -1,1 +1,19 @@
-#Teste
+#Conhecendo o IF
+
+idade = 23
+nome = "Daniel"
+
+#Assim pode
+if(idade > 18)
+	#puts nome
+end
+
+#Mas assim é mais legivel, é chamado de syntax sugar
+if idade > 18
+	#puts nome
+end
+
+#Quando for apenas uma linha de if utilizar essa sintaxe para melhorar a legibilidade
+puts nome if idade > 18
+
+#Valores nulos
