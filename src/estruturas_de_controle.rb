e58abcf4077daeda2656d094de447fbf6c53d5b2
danielpump <new_daniel2010@hotmail.com>
2014-12-09 18:38:04 -0200

    Validações com nil

--- src/estruturas_de_controle.rb
+++ src/estruturas_de_controle.rb
@@ -16,4 +16,42 @@ end
 #Quando for apenas uma linha de if utilizar essa sintaxe para melhorar a legibilidade
 puts nome if idade > 18
 
-#Valores nulos
+#If com valores nulos
+
+#O metodo nil? faz a validação caso o objeto esteja nulo
+
+nome = nil
+
+puts "Olá #{nome} que não aparece porque é nulo" if not nome.nil?
+
+nome = "Daniel"
+
+puts "Olá #{nome} com if not" if not nome.nil?
+
+#Unless funciona como o if not só que mais semantico na leitura
+nome = "Daniel"
+
+puts "Olá #{nome} com unless e nil" unless nome.nil?
+
+nome = "Daniel"
+
+#Se o if for feito só com o objeto ele faz a validação para verificar se ele é nulo por padrão
+puts "Olá #{nome} com unless e só a variavel" unless nome
+
+#Iterações
+
+#For
+
+
+
+
+
+
+
+
+
+
+
+
+
+
