bea6205a945eae636a3de100ed7fedc9cc495f33
danielpump <new_daniel2010@hotmail.com>
2014-12-22 00:27:25 -0200

    Entendendo o que é clojure

---
+++ src/clojure.rb
@@ -0,0 +1,5 @@
+=begin
+  Qualquer bloco de código criado tem acesso as variaveis visiveis no escopo onde ele foi criado, e qualquer alteração realizada nelas
+  será refletida no escopo da chamada dos blocos. 
+  Esta capacidade de acesso as variaveis do escopo original é chamada de clojure
+=end
